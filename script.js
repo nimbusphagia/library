85e9efd77afcbdf50f8d9e10e369004d87bcf9ef
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2025-03-15 18:13:53 -0500

    Refactor using classes, add index validation in book obj

--- script.js
+++ script.js
@@ -1,112 +1,103 @@
-//LIBRARY 
-const myLibrary = [{title:"Los Detectives Salvajes", author:"Roberto Bolaño", published:"1998"}];
-
-//LIBRARY DISPLAY FROM THE ARRAY
-const bookContainer = document.querySelector(".book-container");
-const dialogAdd = document.querySelector("dialog")
-const btnPrompt = document.querySelector(".add");
-const form = document.querySelector("form");
-const btnClose = document.querySelector(".close");
-const counter = document.querySelector(".counter");
-
-function newBookCell(newBook){
-    const div = document.createElement("div");
-    div.className = "book";
-    bookContainer.appendChild(div);
-
-    const title = document.createElement("p");
-    const author = document.createElement("p");
-    const published = document.createElement("p");
-    const btnDel = document.createElement("p");
-    const btnRead = document.createElement("button");
-    btnRead.className = "read";
-    btnDel.className = "delete";
-
-    title.textContent = newBook.title;
-    author.textContent = newBook.author;
-    published.textContent = newBook.published;
-    btnDel.textContent = "×";
-    btnDel.id = myLibrary.length - 1;
-
-    div.appendChild(title);
-    div.appendChild(author);
-    div.appendChild(published);
-    div.appendChild(btnDel);
-    div.appendChild(btnRead);
-}
-
-function displayLibrary(library){
-    const newItem = library[library.length - 1];
-    newBookCell(newItem);
-}
-//ADD BOOK
-btnPrompt.addEventListener("click", ()=>{dialogAdd.showModal()});
-btnClose.addEventListener("click", ()=>{dialogAdd.close()});
-
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const formData = new FormData(form);
-    const newBook = Object.fromEntries(formData);
-    if(newBook.title === "" || newBook.author === ""){
-        dialogAdd.close();
-    } else{
-        myLibrary.push(newBook);
-        displayLibrary(myLibrary);
-        keepCount();
-        dialogAdd.close();
+class Book {
+    constructor(title, author, year, index){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.index = index;
     }
-});
-
-//COUNT
-function keepCount(){
-    counter.innerHTML = myLibrary.length;
 }
+class Library {
+    constructor(){
+        //LIBRARY ARRAY
+        this.books = [];
+        this.newBook("Los Detectives Salvajes", "Roberto Bolaño", 1998, +this.books.length);
+        this.displayBook("Los Detectives Salvajes", "Roberto Bolaño", 1998, this.books.length - 1);
+    }
+    newBook(title, author, year, index){
+        //ADD NEW BOOK TO LIBRARY
+        let book = new Book(title, author, year, index);
+        this.books.push(book);
+    }
+    displayForm(){
+        //OPEN DIALOG
+        const promptBtn = document.querySelector(".add");
+        const dialog = document.querySelector("dialog");
+        const closeBtn = document.querySelector(".close");
+        promptBtn.addEventListener("click", ()=>{dialog.showModal()});
+        closeBtn.addEventListener("click", ()=>{dialog.close()});
+    }
+    bookInput(){
+        //INPUT BOOK AND DISPLAY IT
+        const submitBtn = document.querySelector(".btnContainer > button");
+        submitBtn.addEventListener("click",()=>{
+            let title = document.querySelector("#title").value;
+            let author = document.querySelector("#author").value;
+            let year = document.querySelector("#year").value;
+            if( title === ""|| author === ""|| year === ""){
+                document.querySelector("dialog").close();
+            } else{
+                this.newBook(title, author, +year, +this.books.length);//CREATE
+                this.keepCount();
+                this.displayBook(title, author, year, this.books.length - 1);//DISPLAY
+                document.querySelector("dialog").close();
+            }  
+        });
+    }
+    displayBook(title, author, year, index){
+        // DOM
+        const bookContainer = document.querySelector(".book-container");
+        const div = document.createElement("div");
+        div.className = "book";
+        div.id = index; // FOR BOOK VALIDATION
+        bookContainer.appendChild(div);
 
-//DELETE BOOK
+        const pTitle = document.createElement("p");
+        const pAuthor = document.createElement("p");
+        const pYear = document.createElement("p");
+        const btnDel = document.createElement("p");
+        const btnRead = document.createElement("button");
+        btnRead.className = "read";
+        btnRead.id = "unread";
+        btnDel.className = "delete";
 
-document.body.addEventListener("click", (e)=>{
-    let tracker = "default";
-    let bookArr = [];
-    if(e.target.className == "delete"){
-        tracker = e.target.id;
-        myLibrary.splice(e.target.id, 1);
-        e.target.parentElement.remove();
-        keepCount(myLibrary);
-        bookArr = Array.from(document.querySelectorAll(".delete"));
-        if(tracker !== "default"){
-            for(const del of bookArr){
-                if(del.id > tracker){
-                    del.id--;
-                    console.log(bookArr);
-                }
+        pTitle.textContent = title;
+        pAuthor.textContent = author;
+        pYear.textContent = year;
+        btnDel.textContent = "×";
+
+        div.append(pTitle, pAuthor, pYear, btnDel, btnRead);
+    }
+    alterDisplay(){
+        document.body.addEventListener("click", (e)=>{
+            if(e.target.className == "delete"){
+                //FIND IN THE ARRAY
+                let bookIndex = this.books.findIndex((book)=> book.index == e.target.parentElement.id);
+                //DELETE IT
+                this.books.splice(bookIndex, 1);
+                e.target.parentElement.remove();
+                this.keepCount();
             }
-        }
+            //TOGGLE READ BUTTON
+            if(e.target.id === "read"){
+                e.target.style.background = "url(./icons/grayBook.svg)";
+                e.target.setAttribute("title", "Mark as read");
+                e.target.id = "unread";
+            } else if(e.target.id == "unread"){
+                e.target.style.background = "url(./icons/blackBook.svg)";
+                e.target.setAttribute("title", "Mark as unread");
+                e.target.id = "read";
+            }
+        })
     }
-})
-//READ TOGGLE
-
-document.body.addEventListener("click", (e)=>{
-    if(e.target.className == "read"){
-        let idTrack = e.target.parentElement.querySelector(".delete");
-        if(myLibrary[idTrack.id].read == true){
-            myLibrary[idTrack.id].read = false;
-            e.target.style.background = "url(./icons/book_4_spark_24dp_999999_FILL0_wght400_GRAD0_opsz24.svg)";
-            e.target.setAttribute("title", "Mark as read");
-        } else{
-            e.target.style.background = "url(././icons/off1.svg)";
-            myLibrary[idTrack.id].read = true;
-            e.target.setAttribute("title", "Mark as unread");
-        }
-        console.log(myLibrary[idTrack.id]);
+    keepCount(){
+        document.querySelector(".counter").innerHTML = this.books.length;
     }
-})
+}
 
-
-
-
-
-
-
+const library = new Library();
+library.displayForm();
+library.bookInput();
+library.alterDisplay();
 
 
 
